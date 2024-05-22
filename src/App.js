4015c756ca63891e3232f258325a1af897e33b86
Ashutosh Singh <ashutoshsingh.45987@gmail.com>
2024-05-22 19:37:07 +0530

    Firestore database usage to track expense

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import {Auth} from './pages/auth/index'
-import ExpenseTracker from './pages/expense-tracker';
+import {ExpenseTracker} from './pages/expense-tracker';
 
 function App() {
   return (
